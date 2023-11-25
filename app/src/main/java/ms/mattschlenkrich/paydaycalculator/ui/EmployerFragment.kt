0b7525dbf965e04567528476d18fffcd132e5525
Matt Schenkrich <matt_schl@hotmail.com>
2023-11-25 14:22:43 -0800

    added files working on getting the navigation to work

---
+++ app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/EmployerFragment.kt
@@ -0,0 +1,21 @@
+package ms.mattschlenkrich.paydaycalculator.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ms.mattschlenkrich.paydaycalculator.R
+
+class EmployerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employer, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/StartFragment.kt
@@ -0,0 +1,30 @@
+package ms.mattschlenkrich.paydaycalculator.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ms.mattschlenkrich.paydaycalculator.R
+import ms.mattschlenkrich.paydaycalculator.databinding.FragmentStartBinding
+
+class StartFragment : Fragment(R.layout.fragment_start) {
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
