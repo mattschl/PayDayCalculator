0183c8270abe5e2ff7e6550de64cafef35067d21
Matt Schenkrich <matt_schl@hotmail.com>
2023-12-07 21:57:26 -0800

    loaded onto new Linux machine 2nd run

--- app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/employer/EmployerAddFragment.kt
+++ app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/employer/EmployerAddFragment.kt
@@ -81,6 +81,7 @@ class EmployerAddFragment : Fragment(R.layout.fragment_employer_add) {
                                 lblMainMonthDate.visibility = View.VISIBLE
                                 etMainMonthDate.visibility = View.VISIBLE
                             }
+
                             else -> {
                                 lblMidMonthDate.visibility = View.GONE
                                 etMidMonthDate.visibility = View.GONE
