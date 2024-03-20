f64f4532ca9a3c76fd8df960ae597109ca793cc9
Matt Schenkrich <matt_schl@hotmail.com>
2024-03-19 21:31:07 -0700

    removed last error

--- app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/extras/WorkExtraTypeUpdateFragment.kt
+++ app/src/main/java/ms/mattschlenkrich/paydaycalculator/ui/extras/WorkExtraTypeUpdateFragment.kt
@@ -15,7 +15,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import ms.mattschlenkrich.paydaycalculator.R
 import ms.mattschlenkrich.paydaycalculator.common.ANSWER_OK
-import ms.mattschlenkrich.paydaycalculator.common.CommonFunctions
 import ms.mattschlenkrich.paydaycalculator.common.DateFunctions
 import ms.mattschlenkrich.paydaycalculator.databinding.FragmentWorkExtraTypeUpdateBinding
 import ms.mattschlenkrich.paydaycalculator.model.Employers
@@ -31,7 +30,8 @@ class WorkExtraTypeUpdateFragment : Fragment(
     private lateinit var mView: View
     private lateinit var mainActivity: MainActivity
     private val df = DateFunctions()
-    private val cf = CommonFunctions()
+
+    //    private val cf = CommonFunctions()
     private val extraTypeList = ArrayList<WorkExtraTypes>()
     private lateinit var curEmployer: Employers
     private lateinit var curExtraType: WorkExtraTypes
