2dcd050aebc7daea25b53426345fecfbd7b2cb2c
Matt Schenkrich <matt_schl@hotmail.com>
2025-06-29 17:58:39 -0700

    upgraded agp

--- app/src/main/java/ms/mattschlenkrich/paycalculator/ui/timesheet/timesheetadapter/WorkDateDefaultExtraAdapter.kt
+++ app/src/main/java/ms/mattschlenkrich/paycalculator/ui/timesheet/timesheetadapter/WorkDateDefaultExtraAdapter.kt
@@ -1,13 +1,11 @@
 package ms.mattschlenkrich.paycalculator.ui.timesheet.timesheetadapter
 
-import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import ms.mattschlenkrich.paycalculator.R
 import ms.mattschlenkrich.paycalculator.common.FRAG_WORK_DATE_UPDATE
 import ms.mattschlenkrich.paycalculator.database.model.extras.WorkExtraTypes
 import ms.mattschlenkrich.paycalculator.databinding.ListWorkDateExtraItemBinding
@@ -58,28 +56,29 @@ class WorkDateDefaultExtraAdapter(
             chkExtra.text = extra.wetName
             chkExtra.isChecked = extra.wetIsDefault
             chkExtra.setOnClickListener {
-                confirmSaveWorkDate(chkExtra.isChecked, extra)
+//                changed to automatically saved
+//                confirmSaveWorkDate(chkExtra.isChecked, extra)
+                saveWorkDate(chkExtra.isChecked, extra)
             }
             btnEdit.visibility = View.GONE
         }
     }
 
-    private fun confirmSaveWorkDate(isChecked: Boolean, extra: WorkExtraTypes) {
-        AlertDialog.Builder(mView.context)
-            .setTitle(mView.context.getString(R.string.choose_the_next_step)).setMessage(
-                mView.context.getString(R.string.in_order_to_add_extras_this_work_date_must_be_saved)
-            ).setPositiveButton(
-                mView.context.getString(R.string.save)
-            ) { _, _ ->
-                saveWorkDate(isChecked, extra)
-            }.setNegativeButton(mView.context.getString(R.string.go_back), null).show()
-
-    }
+//    private fun confirmSaveWorkDate(isChecked: Boolean, extra: WorkExtraTypes) {
+//        AlertDialog.Builder(mView.context)
+//            .setTitle(mView.context.getString(R.string.choose_the_next_step)).setMessage(
+//                mView.context.getString(R.string.in_order_to_add_extras_this_work_date_must_be_saved)
+//            ).setPositiveButton(
+//                mView.context.getString(R.string.save)
+//            ) { _, _ ->
+//                saveWorkDate(isChecked, extra)
+//            }.setNegativeButton(mView.context.getString(R.string.go_back), null).show()
+//    }
 
     private fun saveWorkDate(
         isChecked: Boolean, extra: WorkExtraTypes
     ) {
         workDateAddFragment.addToExtraList(isChecked, extra)
-        workDateAddFragment.validateWorkDateToSave(FRAG_WORK_DATE_UPDATE, false)
+        workDateAddFragment.validateWorkDateToSave(FRAG_WORK_DATE_UPDATE, true)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.android.library") version "8.10.1" apply false
+    id("com.android.library") version "8.11.0" apply false
     id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
     id("com.google.dagger.hilt.android") version "2.42" apply false
 }
