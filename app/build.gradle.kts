9d5fb4889891082f54e2ac624aee521c5eaff6e2
Matt Schenkrich <matt_schl@hotmail.com>
2025-08-04 18:53:54 -0700

    Upgrade AGP and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,8 +83,8 @@ dependencies {
     implementation("io.mockk:mockk:1.14.5")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     val roomVersion = "2.7.2"
     implementation("androidx.room:room-runtime:$roomVersion")
@@ -95,7 +95,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("androidx.room:room-ktx:$roomVersion")
 
-    val navVersion = "2.9.2"
+    val navVersion = "2.9.3"
     // Kotlin Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         google()
     }
     dependencies {
-        val navVersion = "2.9.2"
+        val navVersion = "2.9.3"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
@@ -18,9 +18,9 @@ plugins {
     // If your target is Kotlin Multiplatform:
     // kotlin("multiplatform") version "2.1.0"
 
-    id("com.android.application") version "8.11.1" apply false
+    id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("com.android.library") version "8.11.1" apply false
+    id("com.android.library") version "8.12.0" apply false
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
