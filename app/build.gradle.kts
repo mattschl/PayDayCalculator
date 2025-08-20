7a16c5dcf86d50147d7eb7ffef6b95873d403506
Matt Schenkrich <matt_schl@hotmail.com>
2025-08-19 20:46:37 -0700

    upgraded AGP and sone other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,11 +74,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("org.mockito:mockito-core:5.18.0")
+    implementation("org.mockito:mockito-core:5.19.0")
     implementation("org.mockito.kotlin:mockito-kotlin:6.0.0")
     implementation("io.mockk:mockk:1.14.5")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
