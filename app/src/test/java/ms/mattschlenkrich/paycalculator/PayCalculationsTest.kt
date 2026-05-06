348051be014eaa2933d97815dad13c93a3dd8528
Matt Schenkrich <matt_schl@hotmail.com>
2026-05-06 13:54:35 -0700

    upgraded some Gradle dependencies and removed some deprecated options

--- app/src/test/java/ms/mattschlenkrich/paycalculator/PayCalculationsTest.kt
+++ app/src/test/java/ms/mattschlenkrich/paycalculator/PayCalculationsTest.kt
@@ -1,10 +1,10 @@
 package ms.mattschlenkrich.paycalculator
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import ms.mattschlenkrich.paycalculator.data.Employers
-import ms.mattschlenkrich.paycalculator.data.PayPeriods
+import kotlinx.coroutines.runBlocking
+import ms.mattschlenkrich.paycalculator.data.PayCalculationsViewModel
+import ms.mattschlenkrich.paycalculator.data.PayDetailViewModel
+import ms.mattschlenkrich.paycalculator.data.entity.Employers
+import ms.mattschlenkrich.paycalculator.data.entity.PayPeriods
 import ms.mattschlenkrich.paycalculator.logic.PayCalculationsAsync
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,12 +15,11 @@ import org.mockito.junit.MockitoJUnitRunner
 class PayCalculationsTest {
 
     @Mock
-    private val scope = CoroutineScope(Dispatchers.Default)
+    private lateinit var payCalculationsViewModel: PayCalculationsViewModel
 
     @Mock
-    private val mainActivity = MainActivity()
+    private lateinit var payDetailViewModel: PayDetailViewModel
 
-    @Mock
     private val employerMock = Employers(
         -1550413509,
         "Cornerstone",
@@ -34,7 +33,6 @@ class PayCalculationsTest {
         "2024-10-15 20:49:52"
     )
 
-    @Mock
     private val payPeriodMock = PayPeriods(
         -493210422,
         "2025-01-04",
@@ -43,18 +41,15 @@ class PayCalculationsTest {
         "2024-12-22 09:30:31"
     )
 
-
-    private val payCalculationsAsync = PayCalculationsAsync(
-        mainActivity,
-        employerMock,
-        payPeriodMock
-    )
-
-
     @Test
-    fun getPayRateFromDb_answerIs_31_93() {
-        scope.launch {
-            assert(payCalculationsAsync.calculatePayRateFromDb() == 31.93)
-        }
+    fun getPayRateFromDb_answerIs_31_93() = runBlocking {
+        val payCalculationsAsync = PayCalculationsAsync(
+            payCalculationsViewModel,
+            payDetailViewModel,
+            employerMock,
+            payPeriodMock
+        )
+
+        assert(payCalculationsAsync.calculatePayRateFromDb() == 31.93)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val agpVersion by extra("8.1.2")
     repositories {
         google()
     }
     dependencies {
-        val navVersion = "2.9.7"
         classpath(libs.androidx.navigation.safe.args.gradle.plugin)
     }
 }
@@ -18,9 +16,9 @@ plugins {
     // If your target is Kotlin Multiplatform:
     // kotlin("multiplatform") version "2.1.0"
 
-    id("com.android.application") version "9.2.0" apply false
+    id("com.android.application") version "9.2.1" apply false
 //    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
-    id("com.android.library") version "9.2.0" apply false
+    id("com.android.library") version "9.2.1" apply false
     id("com.google.devtools.ksp") version "2.3.7" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
 }
